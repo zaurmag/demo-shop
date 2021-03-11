07c69c7ec402d235048c8a459b339e9fe42911ad
Заур <zaurmag411@gmail.com>
2021-03-12 01:04:55 +0300

    + Авторизация

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { createStore } from 'vuex'
+import auth from './modules/auth.module'
 
 export default createStore({
   state: {
+    message: ''
   },
   mutations: {
+    setMessage (state, message) {
+      state.message = message
+    },
+    clearMessage (state) {
+      state.message = ''
+    }
   },
   actions: {
   },
   modules: {
+    auth
   }
 })
