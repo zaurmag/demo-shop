dba815c4531357b941be6e3adac30ab40cd17fdb
Заур <zaurmag411@gmail.com>
2021-03-20 23:08:30 +0300

    Мелкие правки

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -5,26 +5,28 @@
     </div>
     <h4 class="product-title">{{ product.title }}</h4>
     <div class="text-center">
-      <button class="btn">{{ product.price }}</button>
-      <!--            <div class="product-controls">-->
-      <!--              <button class="btn danger">-</button>-->
-      <!--              <strong>123</strong>-->
-      <!--              <button class="btn primary">+</button>-->
-      <!--            </div>-->
+      <button class="btn" v-if="!controls" @click="controls = true">{{ product.price }}</button>
+      <div v-else class="product-controls">
+        <button class="btn danger">-</button>
+        <strong>123</strong>
+        <button class="btn primary">+</button>
+      </div>
       <small class="small mt-20" :class="{'not-avilable' : !product.count}">{{ product.count ? 'В наличии: ' + product.count + ' шт.' : 'Нет в наличии' }}</small>
     </div>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 export default {
   props: ['products'],
   name: "ProductCard",
   setup (props) {
+    const controls = ref(false)
 
     return {
+      controls,
       products: computed(() => props.products)
     }
   }
--- src/store/modules/cart.module.js
+++ src/store/modules/cart.module.js
@@ -33,12 +33,13 @@ export default {
     }
   },
   actions: {
-    async load ({ dispatch, state } ) {
+    async load ({ dispatch, commit, state } ) {
       try {
         const productsID = Object.keys(state.cartModel).map((id) => {
           return `id=${id}`
         }).join('&')
         const { data } = await axios.get(`/products?${productsID}`)
+        commit('setProducts', data)
         dispatch('update')
       } catch(e) {
         throw e
