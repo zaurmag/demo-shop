cee1e7dc04748113f2aa6ec1bb38ac33b3697d19
Заур <zaurmag411@gmail.com>
2021-05-20 18:17:56 +0300

    Confirm удаления в категории перенес из формы на страницу Category.vue

--- src/components/admin/AdminCategoryForm.vue
+++ src/components/admin/AdminCategoryForm.vue
@@ -20,41 +20,29 @@
     <div v-if="$slots.edit">
       <div v-if="changed">
         <button class="btn primary" type="submit">Обновить</button>
-        <button class="btn danger" type="button" @click="handleReset">Отменить</button>
+        <button class="btn danger" type="button" @click="handleReset">Сбросить</button>
       </div>
 
       <div v-else>
-        <button class="btn danger" type="button" @click="remove">Удалить</button>
+        <button class="btn danger" type="button" @click="$emit('remove')">Удалить</button>
         <button class="btn" type="button" @click="$emit('close')">Закрыть</button>
       </div>
     </div>
   </form>
-
-  <teleport to="body">
-    <app-modal v-if="modal" title="Вы уверены?" @close="modal = false">
-      <button class="btn primary" type="button" @click="isDelete = true">Да</button>
-      <button class="btn danger" type="button" @click="modal = false">Нет</button>
-    </app-modal>
-  </teleport>
 </template>
 
 <script>
 import { useAdminCategoryForm } from '@/use/admin-category'
 import AppModal from '@/components/ui/AppModal'
-import { ref, watch } from 'vue'
 import { useStore } from 'vuex'
-import { useRouter } from 'vue-router'
 
 export default {
-  emits: ['close', 'delete'],
+  emits: ['close','remove'],
   props: ['id', 'initialValues'],
   name: "AdminProductForm",
   setup(props, { emit }) {
     const store = useStore()
     const id = props.id
-    const router = useRouter()
-    const modal = ref(false)
-    const isDelete = ref(false)
 
     const submit = async values => {
       try {
@@ -73,22 +61,8 @@ export default {
       } catch (e) {}
     }
 
-    const remove = () => {
-      modal.value = true
-      watch(isDelete, val => {
-        if (val) {
-          store.dispatch('categories/delete', id)
-          router.push('/admin/categories')
-          modal.value = false
-        }
-      })
-    }
-
     return {
-      ...useAdminCategoryForm(submit, props.initialValues),
-      remove,
-      modal,
-      isDelete
+      ...useAdminCategoryForm(submit, props.initialValues)
     }
   },
   components: {
--- src/views/admin/Category.vue
+++ src/views/admin/Category.vue
@@ -2,7 +2,7 @@
   <AppLoader v-if="loading" />
 
   <app-page v-else-if="category" :title="category.title">
-    <AdminCategoryForm :id="id" :initialValues="initialValues" @close="close">
+    <AdminCategoryForm :id="id" :initialValues="initialValues" @close="close" @remove="confirmLeave = true">
       <template #edit />
     </AdminCategoryForm>
   </app-page>
@@ -10,6 +10,15 @@
   <h3 v-else class="text-center text-white">
     Категории с ID = {{ $route.params.id }} нет.
   </h3>
+
+  <teleport to="body">
+    <AppConfirm
+      v-if="confirmLeave"
+      title="Действительно хотите удалить категорию?"
+      @reject="confirmLeave = false"
+      @confirm="confirm"
+    />
+  </teleport>
 </template>
 
 <script>
@@ -19,6 +28,7 @@ import { onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
 import AppLoader from '@/components/ui/AppLoader'
 import AdminCategoryForm from '@/components/admin/AdminCategoryForm'
+import AppConfirm from '@/components/ui/AppConfirm'
 
 export default {
   name: "Category",
@@ -30,6 +40,7 @@ export default {
     const id = route.params.id
     const category = ref({})
     const initialValues = ref({})
+    const confirmLeave = ref(false)
 
     onMounted(async () => {
       category.value = await store.dispatch('categories/loadOne', id)
@@ -37,18 +48,27 @@ export default {
       loading.value = false
     })
 
+    const confirm = async () => {
+      await store.dispatch('categories/delete', id)
+      confirmLeave.value = false
+      await router.push('/admin/categories')
+    }
+
     return {
       category,
       loading,
       close: () => router.push('/admin/categories'),
       id,
-      initialValues
+      initialValues,
+      confirmLeave,
+      confirm
     }
   },
   components: {
     AppLoader,
     AppPage,
-    AdminCategoryForm
+    AdminCategoryForm,
+    AppConfirm
   }
 }
 </script>
